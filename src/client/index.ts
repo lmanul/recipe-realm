5c8b31cdaa491e7ab92fb0c71d2017280617810a
Manu Cornet <m@ma.nu>
2024-04-21 04:30:59 +0000

    Tiny cleanup

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,8 +1,3 @@
 import Home from '../client/pages/home';
-import Recipe from '../model/recipe';
-import RecipeStore from '../model/recipeStore';
-
-const recipeStore = RecipeStore.getInstance();
-console.log(recipeStore);
 
 new Home().navigate();
